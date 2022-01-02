90feea6bd2cc5c2e6615837bf84a03f4c01e42ad
Henry Miller <mail.henrym+git@gmail.com>
2022-01-01 19:06:23 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+let employee = {
+    name:'Greg',
+    address:'101 Street'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value,};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const e2 = {...employee};
+    delete e2[key]
+    return e2;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
